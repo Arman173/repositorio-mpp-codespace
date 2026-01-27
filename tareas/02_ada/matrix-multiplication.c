3fb2fe339de2acf7099f91b6b47e26fce81e5e9c
Armando Canul <armanhaz173@gmail.com>
2026-01-27 02:30:51 -0600

    adelanto tarea 2

---
+++ tareas/02_ada/matrix-multiplication.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define DEFAULT_DIM 2000
+
+// Estructura para representar una matriz
+typedef struct {
+    int **data;
+    int rows;
+    int cols;
+} Matrix;
+
+void createMatrix(Matrix *m, int rows, int cols) {
+    m->rows = rows;
+    m->cols = cols;
+    m->data = (int **)malloc(rows * sizeof(int *));
+    for (int i = 0; i < rows; i++) {
+        m->data[i] = (int *)malloc(cols * sizeof(int));
+    }
+}
+
+void freeMatrix(Matrix *m) {
+    for (int i = 0; i < m->rows; i++) {
+        free(m->data[i]);
+    }
+    free(m->data);
+}
+
+void initializeMatrix(Matrix *m) {
+    for (int i = 0; i < m->rows; i++) {
+        for (int j = 0; j < m->cols; j++) {
+            m->data[i][j] = rand() % 5 + 1; // Valores aleatorios entre 1 y 5
+        }
+    }
+}
+
+void printMatrix(Matrix *m) {
+    for (int i = 0; i < m->rows; i++) {
+        for (int j = 0; j < m->cols; j++) {
+            printf("%d ", m->data[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main(int argc, char* argv[]) {
+    // creamos las matrices A, B y C
+    Matrix A, B, C;
+
+    // generamos las matrices con dimension por defecto
+    createMatrix(&A, DEFAULT_DIM, DEFAULT_DIM);
+    createMatrix(&B, DEFAULT_DIM, DEFAULT_DIM);
+    createMatrix(&C, DEFAULT_DIM, DEFAULT_DIM);
+
+    // inicializamos las matrices
+    initializeMatrix(&A);
+    initializeMatrix(&B);
+
+    // imprimimos las matrices A y B
+    // printMatrix(&A);
+    // printf("\n");
+    // printMatrix(&B);
+    // printf("\n");
+
+    // estructura para medir el tiempo
+    struct timespec inicio, fin;
+    
+    // Obtener tiempo inicial ---------------------------------------
+    clock_gettime(CLOCK_MONOTONIC, &inicio);
+    
+    // ---- Algoritmo de multiplicación de matrices ----
+    for (int i = 0; i < A.rows; i++) {
+        for (int j = 0; j < B.cols; j++) {
+            C.data[i][j] = 0;
+            for (int k = 0; k < A.cols; k++) {
+                C.data[i][j] += A.data[i][k] * B.data[k][j];
+            }
+        }
+    }
+    // -------- FIN --------
+    
+    // Obtener tiempo final ----------------------------------------
+    clock_gettime(CLOCK_MONOTONIC, &fin);
+
+    // imprimimos el resultado
+    // printMatrix(&C);
+    // printf("\n");
+    
+    // Calcular diferencia
+    long long duracion_ns = (fin.tv_sec - inicio.tv_sec) * 1000000000 + 
+                       (fin.tv_nsec - inicio.tv_nsec);
+    
+    printf("Tiempo: %.3f ms\n", duracion_ns / 1000000.0);
+
+    // limpiamos las matrices
+    freeMatrix(&A);
+    freeMatrix(&B);
+    freeMatrix(&C);
+    
+    return 0;
+}
